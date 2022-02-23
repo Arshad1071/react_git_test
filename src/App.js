9d2a4cf28cd7575d2b3a4bc7f1c88fbf9fd749d0
Arshad1071 <Arshad1071@yahoo.in>
2022-02-23 11:08:08 +0530

    home added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
+import Home from "./Components/Home";
 import "./App.css";
 
 const App = () => {
   return (
-    <div className='App'>App</div>
-  )
-}
+    <div className="App">
+      App
+      <Home />
+    </div>
+  );
+};
 
 export default App;
